69c8fa95fb143bb72fb01f7b961d2800db0c9553
Fariha Promi <farihapromi2611@gmail.com>
2025-04-21 19:50:56 +0600

    Add shadcn ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,25 @@
+import { Route, Routes } from 'react-router-dom';
 import './global.css';
+import SignInForm from './_auth/form/SignInForm';
+import { Home } from './_root/pages';
+import SignupForm from './_auth/form/SignupForm';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/pages/RootLayout';
 
 const App = () => {
-  return <div className='text-3xl font-bold '>App</div>;
+  <main>
+    <Routes>
+      <Route element={<AuthLayout />}>
+        <Route path='/sign-in' element={<SignInForm />} />
+        <Route path='/sign-up' element={<SignupForm />} />
+      </Route>
+
+      {/* private route */}
+      <Route element={<RootLayout />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+  </main>;
 };
 
 export default App;
