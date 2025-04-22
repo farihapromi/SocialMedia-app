cb92840dc0f794ace33d909b7a2232431a60f2f5
Fariha Promi <farihapromi2611@gmail.com>
2025-04-22 20:53:43 +0600

    Add createUser controller

---
+++ server/src/controllers/userController.js
@@ -0,0 +1,4 @@
+import asyncHandler from 'express-async-handler';
+import { createUserAccount } from '../services/userServices';
+//createuser
+export const createUser = asyncHandler(async (req, res) => {});
