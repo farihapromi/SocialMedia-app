d76f3a5c10ff63d3501ab55627d130ddd9638d19
Fariha Promi <farihapromi2611@gmail.com>
2025-04-22 12:15:43 +0600

    Add form styles for signUp

--- client/src/_auth/form/SignupForm.tsx
+++ client/src/_auth/form/SignupForm.tsx
@@ -33,29 +33,92 @@ const SignupForm = () => {
     console.log(values);
   }
   return (
-    <>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
-          <FormField
-            control={form.control}
-            name='username'
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Username</FormLabel>
-                <FormControl>
-                  <Input placeholder='shadcn' {...field} />
-                </FormControl>
-                <FormDescription>
-                  This is your public display name.
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
+    <Form {...form}>
+      <div className='sm:w-420 flex-center flex-col'>
+        {/* Logo + Text side-by-side */}
+        <div className='flex items-center gap-3'>
+          <img
+            src='/assets/images/logo_final.PNG' // No need to include `/public` in the path
+            alt='LinkGram logo'
+            className='w-10 h-10 object-contain'
           />
-          <Button type='submit'>Submit</Button>
-        </form>
-      </Form>
-    </>
+          <p className='text-white text-xl font-semibold'>LinkGram</p>
+        </div>
+
+        <h3 className='h3-bold md:h2-bold pt-5 sm:pt-12 text-2xl font-bold text-white'>
+          Create a new account
+        </h3>
+
+        <p className='small-medium md:base-regular text-white'>
+          To use LinkGram enter your account details
+        </p>
+      </div>
+
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className='flex flex-col gap-5  mt-4'
+      >
+        <FormField
+          control={form.control}
+          name='username'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input type='text' className='shad-input' {...field} />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='Name'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input type='text' className='shad-input' {...field} />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='email'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input type='email' className='shad-input' {...field} />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='password'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type='password' className='shad-input' {...field} />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button className='shad-button_primary' type='submit'>
+          Submit
+        </Button>
+      </form>
+    </Form>
   );
 };
 
