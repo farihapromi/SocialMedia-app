bc118c0573d81f9687885f7b3020578720553ee5
Fariha Promi <farihapromi2611@gmail.com>
2025-04-26 19:54:25 +0600

    Add getPost route

--- server/src/routes/postRouter.js
+++ server/src/routes/postRouter.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import uploads from '../middleware/uploadMiddleware.js';
 
-import { createPost } from '../controllers/postController.js';
+import { createPost, getPosts } from '../controllers/postController.js';
 const postRouter = express.Router();
 postRouter.post('/', uploads.single('postImage'), createPost);
+postRouter.get('/', getPosts);
 export default postRouter;
